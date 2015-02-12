4e8673390c367600219cb0212bda883afdcf24f2
soumyaabhilash <soumyaabhilash85@gmail.com>
2015-02-12 14:41:26 +0530

    opencart core files added

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/MyStore/admin/');
+define('HTTP_CATALOG', 'http://localhost/MyStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/MyStore/admin/');
+define('HTTPS_CATALOG', 'http://localhost/MyStore/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/MyStore/admin/');
+define('DIR_SYSTEM', '/var/www/html/MyStore/system/');
+define('DIR_LANGUAGE', '/var/www/html/MyStore/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/MyStore/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/MyStore/system/config/');
+define('DIR_IMAGE', '/var/www/html/MyStore/image/');
+define('DIR_CACHE', '/var/www/html/MyStore/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/MyStore/system/download/');
+define('DIR_UPLOAD', '/var/www/html/MyStore/system/upload/');
+define('DIR_LOGS', '/var/www/html/MyStore/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/MyStore/system/modification/');
+define('DIR_CATALOG', '/var/www/html/MyStore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'developer');
+define('DB_PASSWORD', 'developer');
+define('DB_DATABASE', 'mystore');
+define('DB_PREFIX', 'ms_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/MyStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/MyStore/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/MyStore/catalog/');
+define('DIR_SYSTEM', '/var/www/html/MyStore/system/');
+define('DIR_LANGUAGE', '/var/www/html/MyStore/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/MyStore/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/MyStore/system/config/');
+define('DIR_IMAGE', '/var/www/html/MyStore/image/');
+define('DIR_CACHE', '/var/www/html/MyStore/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/MyStore/system/download/');
+define('DIR_UPLOAD', '/var/www/html/MyStore/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/MyStore/system/modification/');
+define('DIR_LOGS', '/var/www/html/MyStore/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'developer');
+define('DB_PASSWORD', 'developer');
+define('DB_DATABASE', 'mystore');
+define('DB_PREFIX', 'ms_');
